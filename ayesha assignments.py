55aa217be189feb01f9afa45e4da521ee51c609d
IslamAyesha <88048857+IslamAyesha@users.noreply.github.com>
2021-07-28 14:31:54 +0530

    Day1 assignment submitted

---
+++ ayesha assignments.py
@@ -0,0 +1,10 @@
+
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> val = [int(i) for i in input("Enter the numbers:").split()]
+Enter the numbers:9 8 2 3 4 5
+>>> val.sort()
+>>> val.reverse()
+>>> print(val)
+[9, 8, 5, 4, 3, 2]
+>>>
