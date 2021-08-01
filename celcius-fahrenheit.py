7428056458bee75e158356e792750c6c036efe51
IslamAyesha <88048857+IslamAyesha@users.noreply.github.com>
2021-08-01 14:58:26 +0530

    final project submitted

---
+++ celcius-fahrenheit.py
@@ -0,0 +1,8 @@
+#celsius to fahrenheit convertor
+celsius = input("enter the celsius value: ")
+temp = float(celsius)
+(fahrenheit) = (temp * 9/5) + 32
+print('fahrenheit:' ,fahrenheit)
+
+enter the celsius value: 56
+fahrenheit: 132.8
